3ee1f2fbd7cab24a3c8afb925843cb7fcd55760a
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-12-30 13:02:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import "antd/dist/antd.css";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
-import { Provider } from "react-redux";
+import { Provider } from "
+
+react-redux";
 
 const store = createStore(
   rootReducer,
